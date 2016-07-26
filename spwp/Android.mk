ebb305144219842b874eb1b95778eac54aae37a0
changhongchen <changhongchen@rdamicro.com>
2016-07-26 10:52:06 +0800

    Add sp and wp test

--- spwp/Android.mk
+++ spwp/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := spwp
 LOCAL_SRC_FILES := main.cpp
-LOCAL_SHARED_LIBRARIES += liblog
+LOCAL_SHARED_LIBRARIES += liblog libcutils libutils
 include $(BUILD_EXECUTABLE)
 
--- spwp/main.cpp
+++ spwp/main.cpp
@@ -1,16 +1,95 @@
 #include <stdio.h>
-
-#define LOG_TAG "spwp"
-#include <utils/Log.h>
 #include <utils/RefBase.h>
+
+using namespace android;
+
+class LightClass : public LightRefBase<LightClass>
+{
+    public:
+        LightClass()
+        {
+            printf("LightClass()\n");
+        }
+        ~LightClass()
+        {
+            printf("~LightClass()\n");
+        }
+};
+
+class WeightClass : public RefBase
+{
+    public:
+        void printRefCount()
+        {
+            int32_t strong = getStrongCount();
+            int32_t weak = getWeakRefs()->getWeakCount();
+            printf("Strong:%d, Weak:%d\n", strong, weak);
+        }
+};
+
+class StrongClass : public WeightClass
+{
+    public:
+        StrongClass()
+        {
+            printf("Call %s();\n", __FUNCTION__);
+        }
+        ~StrongClass()
+        {
+            printf("Call %s();\n", __FUNCTION__);
+        }
+};
+
+class WeakClass : public WeightClass
+{
+    public:
+        WeakClass()
+        {
+            extendObjectLifetime(OBJECT_LIFETIME_WEAK);
+            printf("Call %s();\n", __FUNCTION__);
+        }
+        ~WeakClass()
+        {
+            printf("Call %s();\n", __FUNCTION__);
+        }
+};
 
 int main()
 {
-    printf("Hello Android\n");
-    ALOGV("Verbose log");
-    ALOGD("Debug log");
-    ALOGI("Info log");
-    ALOGW("Warn log");
-    ALOGE("Error log");
+    printf("\n\nLightClass test!\n");
+    {
+        LightClass *pLightClass = new LightClass();
+        sp<LightClass> lpOut = pLightClass;
+        printf("RefCount:%d\n", pLightClass->getStrongCount());
+        {
+            sp<LightClass> lpInner = lpOut;
+            printf("RefCount:%d\n", pLightClass->getStrongCount());
+        }
+        printf("RefCount:%d\n", pLightClass->getStrongCount());
+    }
+    printf("\n\nStrongClass test!\n");
+    {
+        StrongClass *pClass = new StrongClass();
+        wp<StrongClass> wpClass = pClass;
+        pClass->printRefCount();
+        {
+            sp<StrongClass> spClass = pClass;
+            spClass->printRefCount();
+        }
+        sp<StrongClass> sp1 = wpClass.promote();
+        printf("sp1:%p\n", sp1.get());
+    }
+    printf("\n\nWeakClass test!\n");
+    {
+        WeakClass *pClass = new WeakClass();
+        wp<WeakClass> wpClass = pClass;
+        pClass->printRefCount();
+        {
+            sp<WeakClass> spClass = pClass;
+            spClass->printRefCount();
+        }
+        sp<WeakClass> sp1 = wpClass.promote();
+        printf("sp1:%p\n", sp1.get());
+    }
     return 0;
 }
